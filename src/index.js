71bc4de8df1c16a47b4c53f4178d2953cc1d6078
maudro <maudro29@gmail.com>
2019-10-10 13:35:05 +0300

    complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let steck = [];
+  let l = str.length;
+  for (let numStr = 0; numStr < str.length; numStr++) {
+    
+    for (let numConfig = 0; numConfig < bracketsConfig.length; numConfig++) {
+      if (bracketsConfig[numConfig][0] == bracketsConfig[numConfig][1]){
+        if (str[numStr] == bracketsConfig[numConfig][0] && steck.slice(-1)== numConfig) {
+          steck.pop();
+          break;
+      } else if (str[numStr] == bracketsConfig[numConfig][0]) {
+        steck.push(numConfig);
+        break;
+      }
+    } 
+      if (str[numStr] == bracketsConfig[numConfig][0]) {
+        steck.push(numConfig);
+        break;
+      } else if (str[numStr] == bracketsConfig[numConfig][1]) {
+          if (steck.slice(-1) == numConfig && steck[0] != null) {
+            steck.pop();
+            break;
+          } else return false;
+      }
+    }
+  }
+  if (steck[0] != null) {
+    return false;
+  }
+  return true;
 }
